08892f96f52b806ceddac043236812ded1b234ac
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-09-05 19:35:19 +0430

    Added AFA.Carousel() for swiper

--- public/assets/js/main-scripts.js
+++ public/assets/js/main-scripts.js
@@ -3,15 +3,44 @@
     var AFRA = {};
 
     /*====== Test ======*/
-    AFRA.Test = function() {
-        console.log("Worked!")
-    }
+    AFRA.Test = function () {
+        console.log("Worked!");
+    };
+
+    /*====== Owl Carousel Setting ======*/
+    AFRA.Carousel = function () {
+        var x = document.getElementsByClassName("el-slider");
+
+        for (var i = 0; i < x.length; i++) {
+            var el = x[i];
+
+            var swiper = el.getElementsByClassName("swiper-container")[0];
+            var nx = el.getElementsByClassName("el-slider-next")[0];
+            var pr = el.getElementsByClassName("el-slider-prev")[0];
+
+            new Swiper(swiper, {
+                slidesPerView: 2,
+                spaceBetween: 10,
+                navigation: {
+                    nextEl: nx,
+                    prevEl: pr,
+                },
+            });
+        }
+
+        var mySwiper = new Swiper(".el-swiper-3", {
+            loop: false,
+            spaceBetween: 24,
+            slidesPerView: 3,
+            slidesPerColumn: 2,
+        });
+    };
 
     // Windows.On.Load
     $(window).on("load", function () {});
 
     // Document.Ready
     $(document).ready(function () {
-        AFRA.Test();
+        AFRA.Test(), AFRA.Carousel();
     });
 })(jQuery);
