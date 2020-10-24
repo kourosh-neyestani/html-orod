cdf6df9165f4606c06d99a93a6819e5ed4425a49
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-10-24 11:52:10 +0330

    FIxed bugs for demo options

---
+++ public/assets/js/demo.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+    $(".afra-demo .afra-demo-close").on("click", function () {
+        $(".afra-demo").removeClass("active");
+    });
+    $(".afra-demo .afra-demo-toggle").on("click", function () {
+        $(".afra-demo").toggleClass("active");
+    });
+    $(".afra-demo-color-1").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "orange");
+        document.documentElement.style.setProperty("--primary-1", "#ff8800");
+    });
+    $(".afra-demo-color-2").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#de5881");
+        document.documentElement.style.setProperty("--primary-1", "rgb(252, 90, 171)");
+    });
+    $(".afra-demo-color-3").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#0b7554");
+        document.documentElement.style.setProperty("--primary-1", "#339266");
+    });
+    $(".afra-demo-color-4").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#7b1f29");
+        document.documentElement.style.setProperty("--primary-1", "darkred");
+    });
+    $(".afra-demo-color-5").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#00b7b8");
+        document.documentElement.style.setProperty("--primary-1", "#339266");
+    });
+    $(".afra-demo-color-6").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#33b5e5");
+        document.documentElement.style.setProperty("--primary-1", "rgb(158, 221, 241)");
+    });
+    $(".afra-demo-color-7").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#1d3268");
+        document.documentElement.style.setProperty("--primary-1", "#132041");
+    });
+    $(".afra-demo-color-8").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#00897b");
+        document.documentElement.style.setProperty("--primary-1", "#004d40");
+    });
+    $(".afra-demo-color-9").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#ff5e14");
+        document.documentElement.style.setProperty("--primary-1", "#bf360c");
+    });
+    $(".afra-demo-color-10").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#ffa000");
+        document.documentElement.style.setProperty("--primary-1", "#f57c00");
+    });
+    $(".afra-demo-color-11").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#c89454");
+        document.documentElement.style.setProperty("--primary-1", "#a37946");
+    });
+    $(".afra-demo-color-12").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#ff69b4");
+        document.documentElement.style.setProperty("--primary-1", "#eb328e");
+    });
+    $(".afra-demo-color-13").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#ac5f33");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-14").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#e79e7e");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-15").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#ffcdb4");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-16").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#e8a384");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-17").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#f9b689");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-18").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#ffcba6");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-19").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#a45223");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-20").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#d88c75");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-21").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#f7c3b6");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-22").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#dc9263");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-23").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#e9b876");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+    $(".afra-demo-color-24").on("click", function () {
+        document.documentElement.style.setProperty("--primary", "#d78150");
+        document.documentElement.style.setProperty("--primary-1", "");
+    });
+});
